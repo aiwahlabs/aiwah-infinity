047cc372fbbbaa94e0adb327b676830138a0fa0b
aiwahlabs <aiwahlabs@gmail.com>
2025-06-01 10:04:18 +0530

    fixed routing issues

--- app/dashboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardSkeleton } from '@/components/ui/skeletons';
-
-export default function Loading() {
-  return (
-    <div className="space-y-4">
-      <DashboardSkeleton />
-    </div>
-  );
-} 
--- app/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { HomePageLoading } from '@/app/components/loading';
-
-export default function Loading() {
-  return <HomePageLoading />;
-} 
